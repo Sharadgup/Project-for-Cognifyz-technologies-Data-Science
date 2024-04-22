aa850eb4901bf7cc697af3ff92b8d4b395cbc265
sharad Gupta <97171885+Sharadgup@users.noreply.github.com>
2024-04-22 03:08:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import pickle
 import sklearn
-
+print(sklearn.__version__)
 # Load the trained model
 model_path = 'restaurant_rating_prediction_model.pkl'  # Update this with the actual path to your model
 
